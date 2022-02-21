56269538913f1cd3273eb4a670a1936f818b7412
scaevitas <matheustran@gmail.com>
2022-02-22 00:15:25 +0100

    init
    the firt commit, creating the project

---
+++ server/index.js
@@ -0,0 +1,20 @@
+const app = require("express")()
+const server = require("http").createServer(app)
+const io = require("socket.io")(server)
+
+function randint(n){
+    return Math.floor(Math.random() * (n));
+};
+
+io.on("connection", socket =>{
+    console.log("connection established with: " + socket.id);
+    
+    socket.on("disconnect", ()=>{
+        console.log(socket.id + " has left")
+    })
+});
+
+
+
+const PORT = process.env.PORT || 9000;
+server.listen(PORT, () => console.log(`\n\x1b[32m[server]\x1b[0m running on port: \x1b[33m${PORT}\x1b[0m \n`));
---
+++ server/misc/cards_create.py
@@ -0,0 +1,20 @@
+def jsonify(text):
+    with open(text, "r") as file:
+        thing = list(file)
+        last = len(thing)-1
+        for i,x in enumerate(thing):
+            card = x.replace("\n", "").replace("/ ", "\\n \\n").replace('"', '\\"')
+            if "Custom" in x or "___" in x:
+                json.write('        {'+f'"text":"{card}", ' + '"Custom": true}' + (", \n" if i != last else "\n"))
+            else:
+                json.write('        {'+f'"text":"{card}", ' + '"Custom": false}' + (", \n" if i != last else "\n"))
+
+if __name__ == "__main__":
+    with open("cards.json", "w") as json:
+        json.write('{ \n    "red": [ \n')
+        jsonify("flags.txt") 
+        json.write('    ],\n    "white":[ \n')
+        jsonify("perks.txt")
+        json.write('    ]\n}')
+
+    print("complete")
