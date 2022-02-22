e96a0f48e105c5e77b4c8a6cbec624f81301dea0
scaevitas <matheustran@gmail.com>
2022-02-22 21:07:54 +0100

    added a pop up to create rooms

--- client/src/components/Rooms.js
+++ client/src/components/Rooms.js
@@ -1,9 +1,16 @@
-import React from 'react'
+import React, {useState, useRef} from 'react'
+import Popup from './Popup'
 
 export default function Rooms() {
+    const roomRef = useRef()
+    const [popupOn, setPopupOn] = useState(false);
     return (
         <div>
-            <div className='btn' datatext="Get_A_room">Get_A_room</div>
+            <div className='btn' datatext="Get_A_room" onClick={()=>setPopupOn(!popupOn)}>Get_A_room</div>
+            <Popup trigger={popupOn} text="create" setTrigger={setPopupOn}>
+                <h1>Room details:</h1>
+                <p><input required ref={roomRef} className="input" name="Lobbyname" id="Lobbyname" placeholder="Lobby name"/></p>
+            </Popup>
         </div>
     )
 }
