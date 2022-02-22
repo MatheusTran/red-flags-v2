2353b690e1c7efa212567d86e5c6c888915e8d7c
scaevitas <matheustran@gmail.com>
2022-02-22 17:12:37 +0100

    removed temp folder

--- fucku/src/components/Rooms.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export default function Rooms() {
-    return (
-        <div>
-            <div className='btn' datatext="Get_A_room">Get_A_room</div>
-        </div>
-    )
-}
