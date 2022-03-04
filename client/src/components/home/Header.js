ea999d4f3214214c43a81ba5f32a477e5ffada3c
scaevitas <matheustran@gmail.com>
2022-03-04 19:16:36 +0100

    updated header

--- client/src/components/home/Header.js
+++ client/src/components/home/Header.js
@@ -1,25 +1,20 @@
-import PropTypes from 'prop-types';
+import React, {useState} from 'react'
 
-const Header = (props) => {
-    return(
-    <header> 
-        <h1 id="title"><a style={antiLink} href={"data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAOEAAADhCAMAAAAJbSJIAAAAA1BMVEX/AAAZ4gk3AAAASElEQVR4nO3BgQAAAADDoPlTX+AIVQEAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAADwDcaiAAFXD1ujAAAAAElFTkSuQmCC"} className="neon">RED</a> FLAGS</h1>
-        <h1 id="subtitle">{props.text}</h1>
-    </header>
-    );
-};
-
-Header.defaultProps = {
-    text: "for some reason I could not pick a random quote"
+export default function Header() {
+    const subs = ["According to all known laws of Aviation, a bee should not be able to fly...","So... You come here often?","Because NBs only date Samurai's from the early 1950s", "Because guys only date bitchy bimbos","Because girls only date assholes", "It's about to you fell in love", "Just give up and settle", "Stop being picky and just choose one", "j;asghklfasa;wnoibersnoiah", "Time to fall in love", "Your best chance at marriage", "Let's face it, you don't have any better options", "Your mom really worries about you", "There's got to be at least one fish in the sea"]
+    const [sub, setSub] = useState(subs[Math.floor(Math.random()*subs.length)])
+    
+    
+    return (
+        <header style={{cursor:"pointer"}} onClick={()=>setSub(subs[Math.floor(Math.random()*subs.length)])}> 
+            <h1 id="title"><a style={antiLink} href={"data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAOEAAADhCAMAAAAJbSJIAAAAA1BMVEX/AAAZ4gk3AAAASElEQVR4nO3BgQAAAADDoPlTX+AIVQEAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAADwDcaiAAFXD1ujAAAAAElFTkSuQmCC"} className="neon">RED</a> FLAGS</h1>
+            <h1 id="subtitle">
+                {sub}
+            </h1>
+        </header>
+    )
 }
-
-Header.propTypes = {
-    text: PropTypes.string.isRequired
-}
-
 const antiLink = {
     textDecoration:"none",
     color:"white"
 }
-
-export default Header;
