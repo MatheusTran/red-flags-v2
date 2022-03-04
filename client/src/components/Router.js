945f53b20725fcbd5c21a907b2ea8ca718f4085e
scaevitas <matheustran@gmail.com>
2022-03-04 19:32:47 +0100

    added some new lines for header

--- client/src/components/Router.js
+++ client/src/components/Router.js
@@ -2,7 +2,7 @@ import React from 'react'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import App from './home/App'
 import Game from "./game/Game"
-
+//note to self: might make a different route for the lobbies
 export default function Router() {
     return (
         <BrowserRouter>
