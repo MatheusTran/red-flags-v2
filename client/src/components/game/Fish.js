8097e458b363f1aad69350661d193ba653b28217
scaevitas <matheustran@gmail.com>
2022-04-06 23:50:30 +0200

    presented fish styling

--- client/src/components/game/Fish.js
+++ client/src/components/game/Fish.js
@@ -3,13 +3,25 @@ import { useData } from './dataProvider'
 
 function Fish() {
     const { presentedFish } = useData()
+    
+    function getProfileStyle(){//I could turn this into a ternary operator like I did with the cards, but it's a bit hard to read. I might make the background a variable
+        if(!presentedFish)return
+        if(!presentedFish.show){
+            if(presentedFish.present){
+                return {background: `linear-gradient(180turn,rgba(0,0,0,0.65),rgba(0,0,0,0)), url(${presentedFish.url})`, opacity:"0.5"}
+            } else{
+                return {display:"none"}
+            }
+        }
+        return {background: `linear-gradient(180turn,rgba(0,0,0,0.65),rgba(0,0,0,0)), url(${presentedFish.url})`}
+    }
     return (
         <>
-            <div className='card presented profile' style={presentedFish.present?{background: `linear-gradient(180turn,rgba(0,0,0,0.65),rgba(0,0,0,0)), url(${presentedFish.url})`}:{display:'none'}}>
+            <div className='card presented profile' style={getProfileStyle()}>
                 <h1 style={{fontSize:"20px", marginTop:"auto", position:"absolute", bottom:"0", left:"0"}}>{presentedFish.name}</h1>
             </div>
             {presentedFish?.cards?.map((card, index)=>(
-                <div key={"fish"+ index} className={card.color + " card presented"} style={presentedFish.present?{opacity:"0.5"}:{display:"none"}}>
+                <div key={"fish"+ index} className={card.color + " card presented"} style={presentedFish.show?{}:presentedFish.present?{opacity:"0.5"}:{display:"none"}}>
                     {card.text}
                 </div>
             ))}
