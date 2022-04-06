f081aa5442b30f1892b7fcfc1678e6e69356f920
scaevitas <matheustran@gmail.com>
2022-04-06 23:27:16 +0200

    some styling for the presenter

--- client/src/components/game/Fish.js
+++ client/src/components/game/Fish.js
@@ -5,11 +5,11 @@ function Fish() {
     const { presentedFish } = useData()
     return (
         <>
-            <div className='card presented' style={{overflow:"hidden",background: `linear-gradient(180turn,rgba(0,0,0,0.65),rgba(0,0,0,0)), url(${presentedFish.url})`, maxWidth:"10.25rem", height:"9rem", backgroundSize:"cover", backgroundPosition:"center", position:"relative"}}>
+            <div className='card presented profile' style={presentedFish.present?{background: `linear-gradient(180turn,rgba(0,0,0,0.65),rgba(0,0,0,0)), url(${presentedFish.url})`}:{display:'none'}}>
                 <h1 style={{fontSize:"20px", marginTop:"auto", position:"absolute", bottom:"0", left:"0"}}>{presentedFish.name}</h1>
             </div>
             {presentedFish?.cards?.map((card, index)=>(
-                <div key={"fish"+ index} className={card.color + " card presented"}>
+                <div key={"fish"+ index} className={card.color + " card presented"} style={presentedFish.present?{opacity:"0.5"}:{display:"none"}}>
                     {card.text}
                 </div>
             ))}
