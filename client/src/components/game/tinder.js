6dbb8b3e4f0a2ba7468607bc1bc849c0d082a249
scaevitas <matheustran@gmail.com>
2022-04-14 13:12:53 +0200

    mock

--- client/src/components/game/tinder.js
+++ client/src/components/game/tinder.js
@@ -4,6 +4,12 @@ import { Profile } from './profile'
 
 function Tinder() {
     const { room } = useData()
+    const mock = [
+        {url:"", name:"test"},
+        {text:"this is the first card"},
+        {text:"this is a white card"},
+        {text:"this is a red card"}
+    ]
     return (
         <>
         {
@@ -11,7 +17,7 @@ function Tinder() {
         <div style={{width:"100%", height:"100%",top:"0", position:"fixed", zIndex:"30", background:"rgba(0,0,0,0.75)", left:"0"}}>
             <div style={{position:"fixed", width:"500px", background:"white", borderRadius:"5%", height:"100%", left:"50%", transform:"translateX(-50%)"}}>
                 {room.order.map((fish, index)=>(
-                    <Profile key={"profile" + index} cards={fish}/>
+                    <Profile key={"profile" + index} cards={fish} name={"mock user"}/>
                 ))}
             </div>
         </div>:""
