e0ec352983276a885bb074cc5d603c1e596123be
scaevitas <matheustran@gmail.com>
2022-04-19 18:21:44 +0200

    possible fix for a bug

--- client/src/components/game/tinder.js
+++ client/src/components/game/tinder.js
@@ -6,7 +6,7 @@ import { faXmark, faHeart } from '@fortawesome/free-solid-svg-icons'
 
 function Tinder() {
     const { room } = useData()
-    const [num, setNum] = useState(0)
+    const [num, setNum] = useState(1)
     const mock = [
     [
         {url:"https://images.unsplash.com/photo-1514924801778-1db0aba75e9b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=934&q=80", name:"test"},
@@ -29,7 +29,7 @@ function Tinder() {
 ]
     function next(){
         setNum(()=>{
-        if(num+1>=mock.length)return 0
+        if(num>=mock.length-1)return 1
         return num+1
         })
     }
@@ -40,13 +40,10 @@ function Tinder() {
         <div style={{width:"100%", height:"100%",top:"0", position:"fixed", zIndex:"30", background:"rgba(0,0,0,0.75)", left:"0"}}>
             <div style={{position:"fixed", width:"500px", background:"white", borderRadius:"5%", height:"100%", left:"50%", transform:"translateX(-50%)"}}>
                 <div style={{display:"grid"}}>
-                    {mock.map((profile, index)=>(
-                        <Profile key={"profile "+index} cards={profile} name={"Matheus"} index={mock.length-index} mount={index>=num}/>
+                    {room.order.map((profile, index)=>(
+                        <Profile key={"profile "+index} cards={profile.fish.cards} name={profile.username} index={mock.length-index} mount={index>=num-1}/>
                     ))}
-                    </div>
-                {/*room.order.map((fish, index)=>(
-                    <Profile key={"profile" + index} cards={fish} name={"mock user"}/>
-                ))*/}
+                </div>
                 <div style={{display:"flex", flexDirection:"row", height:"auto", width:"100%", alignItems:"center", justifyContent:"center"}}>
                     <div className='circle'><FontAwesomeIcon size="2xl" icon={faXmark} style={{color:"rgb(254,133,113)"}} onClick={()=>{next()}}/></div>
                     <div className='circle'><FontAwesomeIcon size="2xl" icon={faHeart} style={{color:"rgb(159, 226,191)"}}/></div>
