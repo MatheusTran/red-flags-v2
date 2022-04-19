50f61f843f4547d71752dec04d2e609a40a0c05d
scaevitas <matheustran@gmail.com>
2022-04-19 18:31:41 +0200

    fixed a strupid mistake

--- client/src/components/game/tinder.js
+++ client/src/components/game/tinder.js
@@ -6,30 +6,12 @@ import { faXmark, faHeart } from '@fortawesome/free-solid-svg-icons'
 
 function Tinder() {
     const { room } = useData()
-    const [num, setNum] = useState(1)
-    const mock = [
-    [
-        {url:"https://images.unsplash.com/photo-1514924801778-1db0aba75e9b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=934&q=80", name:"test"},
-        {text:"this is the first card", color:"white"},
-        {text:"this is a white card", color:"white"},
-        {text:"this is a red card", color:"red"}
-    ],
-    [
-        {url:"https://images.unsplash.com/photo-1522263842439-347f062b8475?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=934&q=80", name:"another one"},
-        {text:"this is the first card", color:"white"},
-        {text:"this is a white card", color:"white"},
-        {text:"this is a red card", color:"red"}
-    ],
-    [
-        {url:"https://cdn.cnn.com/cnnnext/dam/assets/160725131446-graham-car-crash-evolved-human-full-169.jpeg", name:"second person"},
-        {text:"this is another person", color:"white"},
-        {text:"thingy ma bob", color:"white"},
-        {text:"boob", color:"red"}
-    ]
-]
+    const [num, setNum] = useState(0)
+    
     function next(){
         setNum(()=>{
-        if(num>=mock.length-1)return 1
+            console.log(num)
+        if(num+1>=room.order.length)return 0
         return num+1
         })
     }
@@ -41,7 +23,7 @@ function Tinder() {
             <div style={{position:"fixed", width:"500px", background:"white", borderRadius:"5%", height:"100%", left:"50%", transform:"translateX(-50%)"}}>
                 <div style={{display:"grid"}}>
                     {room.order.map((profile, index)=>(
-                        <Profile key={"profile "+index} cards={profile.fish.cards} name={profile.username} index={mock.length-index} mount={index>=num-1}/>
+                        <Profile key={"profile "+index} cards={profile.fish.cards} name={"Matheus"} index={room.order.length-index} mount={index>=num}/>
                     ))}
                 </div>
                 <div style={{display:"flex", flexDirection:"row", height:"auto", width:"100%", alignItems:"center", justifyContent:"center"}}>
