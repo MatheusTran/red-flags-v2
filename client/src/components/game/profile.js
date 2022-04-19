697f4392b8cc69582bb2662638075cd9c8001e9b
scaevitas <matheustran@gmail.com>
2022-04-19 15:44:33 +0200

    swipe animation added

--- client/src/components/game/profile.js
+++ client/src/components/game/profile.js
@@ -1,36 +1,41 @@
 import React from 'react'
 import Carousel from "nuka-carousel";
+import { Transition } from "@mantine/core";
 
 function Profile(props) {
     return (
-        <div style={{textAlign:"left", padding:"1em"}}>
-            <div style={{border:"solid 2px whitesmoke", padding:"0.5em", borderRadius:"5%"}}>
-                <Carousel wrapAround={true}>
-                    <div className="profile-card">
-                        <div className='card presented profile' style={{background: ` url(${props.cards[0]?.url})`,backgroundSize:"cover", backgroundPosition:"center", color:"rgba(0,0,0,0)"}}>
-                            .{/*this period is just to format this card correctly */}
+        <Transition mounted={props.mount} transition="slide-right" duration={500} timingFunction="easeIn">
+            {(styles)=>(
+            <div style={{...styles,textAlign:"left", padding:"1em", gridColumn:"1", gridRow:"1", zIndex:props.index}}>
+                <div style={{border:"solid 2px whitesmoke",background:"white", padding:"0.5em", borderRadius:"5%"}}>
+                    <Carousel wrapAround={true}>
+                        <div className="profile-card">
+                            <div className='card presented profile' style={{background: ` url(${props.cards[0]?.url})`,backgroundSize:"cover", backgroundPosition:"center", color:"rgba(0,0,0,0)"}}>
+                                .{/*this period is just to format this card correctly */}
+                            </div>
                         </div>
-                    </div>
-                    <div className="profile-card">
-                        <div className='white card presented'>
-                            {props.cards[1]?.text}
+                        <div className="profile-card">
+                            <div className='white card presented'>
+                                {props.cards[1]?.text}
+                            </div>
                         </div>
-                    </div>
-                    <div className="profile-card">
-                        <div className='white card presented'>
-                            {props.cards[2]?.text}
+                        <div className="profile-card">
+                            <div className='white card presented'>
+                                {props.cards[2]?.text}
+                            </div>
                         </div>
-                    </div>
-                    <div className="profile-card">
-                        <div className='red card presented'>
-                            {props.cards[3]?.text}
+                        <div className="profile-card">
+                            <div className='red card presented'>
+                                {props.cards[3]?.text}
+                            </div>
                         </div>
-                    </div>
-                </Carousel>
-                <h1 style={{color:"black", marginBottom:"0"}}>{props.cards[0]?.name}</h1>
-                <h3 style={{color:"gray", marginTop:"0"}}>brought to you by {props.name}</h3>
+                    </Carousel>
+                    <h1 style={{color:"black", marginBottom:"0"}}>{props.cards[0]?.name}</h1>
+                    <h3 style={{color:"gray", marginTop:"0"}}>brought to you by {props.name}</h3>
+                </div>
             </div>
-        </div>
+            )}
+        </Transition>
     )
 }
 
--- client/src/components/game/tinder.js
+++ client/src/components/game/tinder.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { useData } from './dataProvider'
 import Profile from './profile'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
@@ -6,24 +6,41 @@ import { faXmark, faHeart } from '@fortawesome/free-solid-svg-icons'
 
 function Tinder() {
     const { room } = useData()
-    const mock = [
+    const [num, setNum] = useState(0)
+    const mock = [[
         {url:"https://cdn.cnn.com/cnnnext/dam/assets/160725131446-graham-car-crash-evolved-human-full-169.jpeg", name:"test"},
         {text:"this is the first card", color:"white"},
         {text:"this is a white card", color:"white"},
         {text:"this is a red card", color:"red"}
-    ]
+    ],
+    [
+        {url:"https://cdn.cnn.com/cnnnext/dam/assets/160725131446-graham-car-crash-evolved-human-full-169.jpeg", name:"second person"},
+        {text:"this is another person", color:"white"},
+        {text:"thingy ma bob", color:"white"},
+        {text:"boob", color:"red"}
+    ]]
+    function next(){
+        setNum(()=>{
+        if(num+1>=mock.length)return 0
+        return num+1
+        })
+    }
     return (
         <>
         {
         room?.data.state!=="pick"?
         <div style={{width:"100%", height:"100%",top:"0", position:"fixed", zIndex:"30", background:"rgba(0,0,0,0.75)", left:"0"}}>
             <div style={{position:"fixed", width:"500px", background:"white", borderRadius:"5%", height:"100%", left:"50%", transform:"translateX(-50%)"}}>
-                <Profile cards={mock} name={"Matheus"}/>
+                <div style={{display:"grid"}}>
+                    {mock.map((profile, index)=>(
+                        <Profile key={"profile "+index} cards={profile} name={"Matheus"} index={index*-1} mount={index>=num}/>
+                    ))}
+                    </div>
                 {/*room.order.map((fish, index)=>(
                     <Profile key={"profile" + index} cards={fish} name={"mock user"}/>
                 ))*/}
                 <div style={{display:"flex", flexDirection:"row", height:"auto", width:"100%", alignItems:"center", justifyContent:"center"}}>
-                    <div className='circle'><FontAwesomeIcon size="2xl" icon={faXmark} style={{color:"rgb(254,133,113)"}}/></div>
+                    <div className='circle'><FontAwesomeIcon size="2xl" icon={faXmark} style={{color:"rgb(254,133,113)"}} onClick={()=>{next()}}/></div>
                     <div className='circle'><FontAwesomeIcon size="2xl" icon={faHeart} style={{color:"rgb(159, 226,191)"}}/></div>
                 </div>
             </div>
